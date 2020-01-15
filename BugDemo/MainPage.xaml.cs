709666a33221758fa88ab993503177f8bb9098de
Oliver Brown <oliver.brown@xdesign.com>
2020-01-15 12:18:58 +0000

    Initial commit.

---
+++ BugDemo/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace BugDemo
+{
+    public partial class MainPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        async void NextButton_Clicked(System.Object sender, System.EventArgs e)
+        {
+            await Detail.Navigation.PushAsync(new NextPage());
+        }
+
+        void MenuButton_Clicked(System.Object sender, System.EventArgs e)
+        {
+            RootPage.IsPresented = !RootPage.IsPresented;
+        }
+    }
+}
